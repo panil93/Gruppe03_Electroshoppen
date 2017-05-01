a10277adc7deb7ea97611f523b32a9bf02e423de
eirikurt1 <eitho16@student.sdu.dk>
2017-05-01 23:50:54 +0200

    Ændringer i Betaling klassen
    Ændret kunde og ordre variablerne i Betaling klassen til private.

--- src/gruppe03_electroshoppen/Betaling.java
+++ src/gruppe03_electroshoppen/Betaling.java
@@ -7,32 +7,45 @@ package gruppe03_electroshoppen;
 
 import java.util.Date;
 
-
 /**
  *
  * @author Laura
  */
 public class Betaling {
-    private double beløb;
-    private Date dato;
-    Kunde kunde;
-    Order order;
-    public Betaling(double beløb, Date dato, Kunde kunde, Order order){
-    this.beløb = beløb;
-    this.dato=dato;
-    this.kunde= kunde;
-    this.order = order;
-}
-    public double getBeløb(){
-    return this.beløb;
-}
-    public Date getDato(){
-        return this.dato;
-    }
-    public Kunde getKundetilBetaling(){
-        return this.kunde;
-    }
-    public Order getOrderTilBetaling(){
-        return this.order;
-    }
+
+	private double beløb;
+	private Date dato;
+	private Kunde kunde;
+	private Order order;
+
+	public Betaling(double beløb, Date dato, Kunde kunde, Order order) {
+
+		this.beløb = beløb;
+		this.dato = dato;
+		this.kunde = kunde;
+		this.order = order;
+	}
+
+	public double getBeløb() {
+
+		return this.beløb;
+	}
+
+	public Date getDato() {
+		
+		return this.dato;
+		
+	}
+
+	public Kunde getKundetilBetaling() {
+		
+		return this.kunde;
+		
+	}
+
+	public Order getOrderTilBetaling() {
+		
+		return this.order;
+		
+	}
 }
